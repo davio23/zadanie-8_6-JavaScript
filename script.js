d0f38de227fc00df0ba3cb1bf3195207edf35c58
davio23 <33763332+davio23@users.noreply.github.com>
2017-11-28 15:35:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var a=6;
-var h=7; 
-var triangleArea=a*h/2; 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var a=1; 
+var b=2; 
+var value=(a * a) - (2 * a * b) + (b * b); 
+console.log (value) 
+if (value >0) { 
+  console.log ("Wynik dodatni") } 
+else if (value < 0) { 
+  console.log("Wynik ujemny") } 
+else if (value==0) { 
+  console.log("Wynik ani dodatni, ani ujemny") }
